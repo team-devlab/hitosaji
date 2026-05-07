7e7cb1100b37921889a1b97658c4bf65191badf3
ittyi <vioiv.k4@gmail.com>
2026-05-07 13:45:48 +0900

    fix typo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
     return (
         <Stack>
-            <Stack.Screen name="index" options={{ title: 'Wellcome' }} />
+            <Stack.Screen name="index" options={{ title: 'Welcome' }} />
             <Stack.Screen name="hello-world" options={{ title: 'Hello World' }} />
             <Stack.Screen name="album" options={{ title: 'アルバム' }} />
             <Stack.Screen name="new-bookmark" options={{ title: 'しおりを残す' }} />
